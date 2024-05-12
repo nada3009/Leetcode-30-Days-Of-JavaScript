e3fc27618fad23b5c3fdcbe9e433c37faef19e50
Nada Ahmad <nada.ahmad3009@gmail.com>
2024-05-12 22:00:13 +0530

    Added solutions to closures problems

---
+++ 4_Counter_II.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    const initial = init;
+    const increment = () => {
+        return ++init;
+    };
+    const decrement = () => {
+        return --init;
+    };
+    const reset = () => {
+        init = initial;
+        return init;
+    };
+    return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
